50b17b3b1738a570121996ea6ce7a42d2d519b25
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-08-22 15:24:55 +0300

    add playSelection function

--- java.js
+++ java.js
@@ -12,3 +12,20 @@ function getComputerChoice() {
 
 const selectedOption = getComputerChoice();
 console.log("Selected Option:", selectedOption)
+
+function playSelection() {
+    const userInput = prompt("Select an option: Rock, Paper or Scissors");
+
+    if (userInput === "Rock") {
+        return "You selected Rock!";
+    } else if (userInput === "Paper") {
+        return "You selected Paper!";
+    } else if (userInput === "Scissors") {
+        return "You selected Scissors!";
+    } else {
+        return "Invalid input. Please select Rock, Paper or Scissors";
+    }
+}
+
+const userOption = playSelection();
+console.log("You selected:", userOption)
