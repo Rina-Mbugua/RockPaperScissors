20ff4b5cd9af34a8573814e896e8f67b5e2feefe
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-08-23 11:57:58 +0300

    Change userOption to an array

--- java.js
+++ java.js
@@ -14,18 +14,16 @@ const selectedOption = getComputerChoice();
 console.log("Selected Option:", selectedOption)
 
 function playSelection() {
-    const userInput = prompt("Select an option: Rock, Paper or Scissors");
+    const userOption = prompt("Select an option: Rock, Paper or Scissors").toLowerCase(); //Convert to lowercase. userOption is the prompt that will contain the user's answer  
+    const validOptions = ["rock", "paper", "scissors"]; // Valid options in lowercase .validOption is the array that contains the valid options
 
-    if (userInput === "Rock") {
-        return "You selected Rock!";
-    } else if (userInput === "Paper") {
-        return "You selected Paper!";
-    } else if (userInput === "Scissors") {
-        return "You selected Scissors!";
-    } else {
-        return "Invalid input. Please select Rock, Paper or Scissors";
+    if (validOptions.includes(userOption)) { // if the the option the user selected is INCLUDED in the array.
+        const selectedOption = userOption.charAt(0).toUpperCase() + userOption.slice(1); // Retreive the character which is the 0th index (The first character always starts at zero) and capitalize it ending at the 1st index (which is the second character)
+        return `You selected ${selectedOption}!`; // we are using the `` backticks here because it is a template literal. This allows for multiline stings, string interpolation with embedded expressions and special constructs. In this case we are creating a string by doing a substitution of a placeholder, which is a nother variable in out case.
+    }   else {
+        return "Invalid input. Please select Rock, Paper or Scissors."
     }
-}
 
+}
 const userOption = playSelection();
 console.log("You selected:", userOption)
