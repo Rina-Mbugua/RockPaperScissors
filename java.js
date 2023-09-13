2cb4f98ec28260048563224d4859484ef8de62f5
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-13 14:52:34 +0300

    added running score logic

--- java.js
+++ java.js
@@ -13,6 +13,9 @@ function getComputerChoice() {
 let userOption;
 let computerOption;
 
+let userScore = 0;
+let computerScore = 0;
+
 let roundsPlayed = 0;
 
 function playRound(userOption, computerOption) {
@@ -20,6 +23,7 @@ function playRound(userOption, computerOption) {
     console.log("Player's Choice: " + userOption);
     console.log("Computer's Choice: " + computerOption);
 
+    let result;
 
     if (computerOption === "rock") {
         if (userOption === "scissors") {
@@ -47,6 +51,11 @@ function playRound(userOption, computerOption) {
         }
     }
 
+    const roundWinner = document.createElement('div');
+        roundWinner.textContent = result;
+
+    const container = document.getElementById('resultContainer')
+    container.appendChild(roundWinner)
 
     console.log("Result: " + result);
 
@@ -54,7 +63,20 @@ function playRound(userOption, computerOption) {
     if (roundsPlayed === 5) {
         console.log("Game Over!")
     }
+
+
+ if (result === "You Win!") {
+    userScore++
+ } else if (result === "You Lose!") {
+    computerScore++
+ }
+
+ console.log(userScore)
+ console.log(computerScore)
 }
+
+
+
 
 const rockBtn = document.getElementById('rock-Btn');
     rockBtn.addEventListener('click', () => playRound("rock", getComputerChoice()));
