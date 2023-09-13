3b36f3a66ea94d0787d186c10d3a8a395fe49e6d
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-13 10:52:30 +0300

    use eventListener to trigger one game

--- java.js
+++ java.js
@@ -10,48 +10,10 @@ function getComputerChoice() {
     }
 }
 
+let userOption;
+let computerOption;
 
-let userScore = 0;
-let computerScore = 0;
-
-
-    //Get computer's choice 
-    const computerOption = getComputerChoice();
-    console.log("Computer's Choice:", computerOption)
-
-    // Get user's choice
-    function playSelection() {
-        let userOption;
-
-        const rockBtn = document.getElementById('rock-Btn');
-        rockBtn.addEventListener('click', () => {
-            const userOption = "rock";
-            console.log("Button clicked. Output: " + userOption);
-        });
-    
-    const paperBtn = document.getElementById('paper-Btn');
-        paperBtn.addEventListener('click', () => {
-            const userOption = "paper";
-            console.log("Button clicked. Output: " + userOption);
-        });
-    
-    const scissorsBtn = document.getElementById('scissors-Btn')
-        scissorsBtn.addEventListener('click', () => {
-            const userOption = "scissors"
-            console.log("Button clicked. Output: " + userOption);
-        }); 
-
-        const validOptions = ["rock", "paper", "scissors"]; // Valid options in lowercase .validOption is the array that contains the valid options
-    
-        if (validOptions.includes(userOption)) { // if the the option the user selected is INCLUDED in the array.
-            const userInput = userOption.charAt(0).toUpperCase() + userOption.slice(1); // Retreive the character which is the 0th index (The first character always starts at zero) and capitalize it ending at the 1st index (which is the second character)
-            return { message: `You selected ${userInput}!`, option: userOption}; // we are using the `` backticks here because it is a template literal. This allows for multiline stings, string interpolation with embedded expressions and special constructs. In this case we are creating a string by doing a substitution of a placeholder, which is a nother variable in out case.
-        }   else {
-            return { message:"Invalid input. Please select Rock, Paper or Scissors.", option: null}
-        }
-    }
-
-    //Determine Winner    
+    //Determine Winner logic
     function playRound(userOption, computerOption) {
         if (computerOption === "rock") {
             if (userOption === "scissors") {
@@ -59,7 +21,7 @@ let computerScore = 0;
             } else if (userOption === "paper") {
                 return "You Win!";
             } else {
-                return "It's a draw!";
+               return "It's a draw!";
             }
         } else if (computerOption === "scissors") {
             if (userOption === "rock") {
@@ -80,8 +42,62 @@ let computerScore = 0;
         }
     }
 
-    const { message, option: userOption } = playSelection();
-    console.log(message);
+    //user & and computer selection
+        const rockBtn = document.getElementById('rock-Btn');
+        rockBtn.addEventListener('click', () => {
+            userOption = "rock";
+            computerOption = getComputerChoice();
+            console.log("Player's Choice: " + userOption);
+            console.log("Computer's Choice: " + computerOption);
+            const result = playRound(userOption, computerOption);
+            console.log(result)
+        });
+    
+    const paperBtn = document.getElementById('paper-Btn');
+        paperBtn.addEventListener('click', () => {
+            userOption = "paper";
+            computerOption = getComputerChoice();
+            console.log("Player's Choice: " + userOption);
+            console.log("Computer's Choice: " + computerOption);
+            const result = playRound(userOption, computerOption);
+            console.log(result)
+        });
+    
+    const scissorsBtn = document.getElementById('scissors-Btn')
+        scissorsBtn.addEventListener('click', () => {
+            userOption = "scissors";
+            computerOption = getComputerChoice();
+            console.log("Player's Choice: " + userOption);
+            console.log("Computer's Choice: " + computerOption);
+            const result = playRound(userOption, computerOption);
+            console.log(result)
+        }); 
+
+//let userScore = 0;
+//let computerScore = 0;
+
+
+    
+
+        //const validOptions = ["rock", "paper", "scissors"]; // Valid options in lowercase .validOption is the array that contains the valid options
+    
+       // if (validOptions.includes(userOption)) { // if the the option the user selected is INCLUDED in the array.
+       //     const userInput = userOption.charAt(0).toUpperCase() + userOption.slice(1); // Retreive the character which is the 0th index (The first character always starts at zero) and capitalize it ending at the 1st index (which is the second character)
+       //     return { message: `You selected ${userInput}!`, option: userOption}; // we are using the `` backticks here because it is a template literal. This allows for multiline stings, string interpolation with embedded expressions and special constructs. In this case we are creating a string by doing a substitution of a placeholder, which is a nother variable in out case.
+      //  }   else {
+      //      return { message:"Invalid input. Please select Rock, Paper or Scissors.", option: null}
+     //   }
+    
+
+    //Determine Winner    
+    
+
+    
+  
+    
+
+   // const { message, option: userOption } = playSelection();
+   // console.log(message);
 
    // const roundWinner = playRound(userOption, computerOption);
     //console.log(`Round ${round} Winner: ${roundWinner}`);
@@ -95,5 +111,5 @@ let computerScore = 0;
    // console.log(`Scores after Round ${round}: You - ${userScore} | Computer - ${computerScore}`);
 
 
-console.log("Final Scores:");
-console.log(`You: ${userScore} | Computer: ${computerScore}`);
+//console.log("Final Scores:");
+//console.log(`You: ${userScore} | Computer: ${computerScore}`);
